e9e3c461c6502011bd86ca73a781244d83c1e4e6
Chris Harmon <charmon@confluent.io>
2023-07-25 09:45:00 -0500

    tweaked sample data
    - aligned file names w/ Mongo collections
    - originally used mgodatagen to generate fake users/customers, but it can't keep integers unique, so I ended up with several different customers with the same user_id. For now, replacing mgodatagen use with a static list of 50 dummy customers with sequential user_id values.

--- src/run_example.sh
+++ src/run_example.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# Generate some random users
-mgodatagen -f "users_config.json" \
-    --uri="mongodb+srv://user:password@my.cluster.mongodb.net/"
-
-# Note: I had envisioned trying to use mgodatagen to generate ALL the sample data,
-# but this proved more challenging than desired for the sake of this demo particularly because
-# I can't make it generate unique ID values.
-# So I gave up and created some sample data which I will mongoimport instead.
+# Import users/customers
+mongoimport --uri="mongodb+srv://user:password@my.cluster.mongodb.net/" \
+    -d coffeeshop \
+    -c products \
+    --file="coffeeshop.users.json" --jsonArray
 
 # Import menu items
 mongoimport --uri="mongodb+srv://user:password@my.cluster.mongodb.net/" \
     -d coffeeshop \
     -c products \
-    --file="panera_menu.json" --jsonArray
+    --file="coffeeshop.menu.json" --jsonArray
 
 # Import orders
 mongoimport --uri="mongodb+srv://user:password@my.cluster.mongodb.net/" \
     -d coffeeshop \
     -c orders \
-    --file="panera_orders.json" --jsonArray
+    --file="coffeeshop.orders.json" --jsonArray
